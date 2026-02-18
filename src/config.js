62913359274e5f8e99ea0ada7fa07f1a6114bd44
Karanperiyapatna <121006514+Karanperiyapatna@users.noreply.github.com>
2026-02-18 09:12:46 +0530

    updated config and EmpOtherservice

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 // src/config.js
 const config = {
+<<<<<<< HEAD
+    API_BASE_URL: "http://15.207.99.54:8000", // Change once, works everywhere. Backend Public IPv4 address Paste here..
+=======
     API_BASE_URL: "http://3.109.55.30:8000", // Change once, works everywhere
+>>>>>>> b59d2a5f1cc141772bc39cc14e7dc077cc3aebae
 };
 
 export default config;
